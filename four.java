f4735ddb73b6d3eb136af0308c8fc4e9b9371854
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-11 21:39:28 +0530

    four

--- four.java
+++ four.java
@@ -1,17 +1,23 @@
 public class four {
     public static void main(String[] args) {
         int arr [] = {5,6,3,1,3,0};
-        int min = Integer.MAX_VALUE;
-        int sec = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int sec = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE;
         for(int i = 0; i< arr.length; i++){
-            if(arr[i]<min){
-                sec= min;
-                min = arr[i];
+            if(arr[i]>max){
+                third = sec;
+                sec= max;
+                max = arr[i];
             }
-            else if(arr[i] < sec && arr[i] != min){
+            else if(arr[i] > sec && arr[i] != max){
+                third = sec;
                 sec = arr[i];
             }
+            else if(arr[i] > third && arr[i] != sec && arr[i] != max){
+                third = arr[i];
+            }
         }
-        System.out.println(sec);
+        System.out.println(third);
     }
 }
