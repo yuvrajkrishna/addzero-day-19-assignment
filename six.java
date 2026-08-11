9321894db2fd52c71a93ff0a29d3f52f7a664271
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-11 21:46:06 +0530

    six

--- six.java
+++ six.java
@@ -1,3 +1,12 @@
 public class six {
-    
+    public static void main(String[] args) {
+        int arr [] = {5,4,3,4,2,1};
+        int n = 5 ;
+        int needSum = (n*(n+1))/2;
+        int presentSum = 0;
+        for(int i = 0 ; i < arr.length; i++){
+            presentSum += arr[i];
+        }
+        System.out.println(presentSum - needSum);
+    }
 }
